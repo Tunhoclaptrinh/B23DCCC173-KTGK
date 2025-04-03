eadd70cf36f0855a06bebbb4bfcdb5ecbcdb5ebf
Tunhoclaptrinh <tuannguyentien16@gmail.com>
2025-04-03 11:00:55 +0700

    add description use tiny editor

--- src/services/ClassRoomManagement/typing.s.d.ts
+++ src/services/ClassRoomManagement/typing.s.d.ts
@@ -1,13 +1,14 @@
 declare module Classroom {
-    // Loại phòng (Chọn từ enum)
-    export type RoomType = 'Lecture' | 'Lab' | 'Auditorium';
-    
-    export interface Record {
-    id: string; // UUID primary key
-    roomCode: string;
-    roomName: string;
-    seatingCapacity: number;
-    roomType: RoomType;
-    assignedStaff: string;
-    }
+	// Loại phòng (Chọn từ enum)
+	export type RoomType = 'Lecture' | 'Lab' | 'Auditorium';
+
+	export interface Record {
+		id: string; // UUID primary key
+		roomCode: string;
+		roomName: string;
+		seatingCapacity: number;
+		roomType: RoomType;
+		assignedStaff: string;
+		description: string; // Added description field
+	}
 }
